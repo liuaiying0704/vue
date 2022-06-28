182d63801207cc5d3802ab5eaaf3eacb0e6eaba5
liuaiying <1425577366@qq.com>
2022-06-28 21:30:44 +0800

    v-on

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
   <div>
-    <a v-bind:href="href"></a>
-    <!-- 在js里面写的就必须import引入图片。 data中： imgLocal: imgLocal, -->
-    <img v-bind:src="imgUrl" />
-    <!-- 在此处直接用相对路径地址是可以的-->
-    <img v-bind:src="imgLocal" />
-    <img src="./assets/logo.png" alt="" />
+    <p>商品总数：{{ count }}</p>
+    <button v-on:click="count++">+1</button>
+    <button v-on:click="addFn">+1</button>
+    <button v-on:click="addFn1(5)">+5</button>
+    <!-- v-on:可写为@ -->
+    <button @click="subFn">-1</button>
   </div>
 </template>
 
 <script>
-// 目标：给原生的属性绑定vue变量
-// - 语法：`v-bind:属性名="vue变量"`
-// - 简写：`:属性名="vue变量"`
-// 总结:给标签设置【属性】  v-bind:属性名="变量" 或者 :属性名="变量"
-import imgLocal from './assets/logo.png';
 export default {
   data() {
     return {
-      href: 'https://www.hao123.com/?src=from_pc',
-      imgUrl:
-        'http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg',
-      imgLocal: imgLocal,
+      count: 0,
     };
   },
+  methods: {
+    addFn() {
+      this.count++;
+    },
+    addFn1(val) {
+      this.count += val;
+    },
+    subFn() {
+      this.count--;
+    },
+  },
 };
+//  1. 绑定事件
+//   语法: v-on:事件名="少量代码"
+//   语法: v-on:事件名="methods里函数名"
+//   语法: v-on:事件名="methods里函数名(值)"
+//   语法: @事件名="methods里函数名"
 </script>
-
 <style></style>
