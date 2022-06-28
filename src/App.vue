0c43700a9251b46b56edc4856f481313a9317c8b
liuaiying <1425577366@qq.com>
2022-06-28 21:17:33 +0800

    给原生的属性绑定vue变量

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,30 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    <h1>{{ obj.name }}</h1>
-    <h1>{{ obj.age }}</h1>
-    <h1>{{ obj.age >= 18 ? '成年' : '未成年' }}</h1>
+    <a v-bind:href="href"></a>
+    <!-- 在js里面写的就必须import引入图片。 data中： imgLocal: imgLocal, -->
+    <img v-bind:src="imgUrl" />
+    <!-- 在此处直接用相对路径地址是可以的-->
+    <img v-bind:src="imgLocal" />
+    <img src="./assets/logo.png" alt="" />
   </div>
 </template>
 
 <script>
+// 目标：给原生的属性绑定vue变量
+// - 语法：`v-bind:属性名="vue变量"`
+// - 简写：`:属性名="vue变量"`
+// 总结:给标签设置【属性】  v-bind:属性名="变量" 或者 :属性名="变量"
+import imgLocal from './assets/logo.png';
 export default {
   data() {
-    //temlate中的变量，在data函数返回的对象里定义
-    //  this指向当前的vue的实例。
     return {
-      msg: 'Hello, 小vue !',
-      obj: {
-        name: 'vue',
-        age: 7,
-      },
+      href: 'https://www.hao123.com/?src=from_pc',
+      imgUrl:
+        'http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg',
+      imgLocal: imgLocal,
     };
   },
 };
-// 目标：插值表达式
-// 库:  封装的属性或方法 (例jquery.js ==》方法的集合
-// 框架: 拥有自己的规则和元素, 比库强大的多 (例vue.js)
-// - vue 是什么 ? Vue是一个javascript渐进式框架
-// - 什么是渐进式呢?  渐进式就是逐渐使用, 集成更多的功能
-// - 什么是库和框架呢?  库是方法的集合, 而框架是一套拥有自己规则的语法
-
-// 语法: {{ 表达式 }}
-// 作用可以把表达式的返回值 直接 插入到dom 标签中
-// 又叫: 声明式渲染/文本插值
 </script>
 
 <style></style>
