4a12ca94a3a4df19d704c8fd8806a63b4f0137cb
liuaiying <1425577366@qq.com>
2022-07-06 18:02:29 +0800

    App.vue内容复制

---
+++ src/08.第4-8的App.vue的内容.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <!-- 04   插槽 -->
+    <UseSlot></UseSlot>
+
+    <!-- 05  具名插槽 -->
+    <UseSlot1></UseSlot1>
+
+    <!-- 06  作用域插槽，使用子组件的值 -->
+    <UseSlot2></UseSlot2>
+
+    <!-- 07  作用域插槽，使用场景 -->
+    <UseSlot3></UseSlot3>
+
+    <!-- 08  作用域插槽，使用场景 -->
+    <UseSlot4>
+      <template v-slot:index="scope">
+        {{ scope.row.index + 1 }}
+      </template>
+
+      <template v-slot:name="scope">
+        {{ scope.row.name }}
+      </template>
+
+      <template v-slot:age="scope">
+        <input type="text" v-model="scope.row.age" />
+      </template>
+
+      <template v-slot:toux="scope">
+        <img :src="scope.row.headImgUrl" alt="" />
+      </template>
+    </UseSlot4>
+  </div>
+</template>
+
+<script>
+import UseSlot from './04_UseSlot.vue';
+
+import UseSlot1 from './05_UseSlot.vue';
+
+import UseSlot2 from './06_UseSlot.vue';
+
+import UseSlot3 from './07.作用域插槽使用场景.vue';
+import UseSlot4 from './08.作用域插槽使用场景.vue';
+
+export default {
+  components: {
+    UseSlot,
+    UseSlot1,
+    UseSlot2,
+    UseSlot3,
+    UseSlot4,
+  },
+};
+</script>
+
+<style></style>
