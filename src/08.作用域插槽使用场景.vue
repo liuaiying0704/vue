78726d4f2662cab4b50205a074e8962087f79b3e
liuaiying <1425577366@qq.com>
2022-07-06 20:27:30 +0800

    更新08

--- src/08.作用域插槽使用场景.vue
+++ src/08.作用域插槽使用场景.vue
@@ -1,9 +1,56 @@
 <template>
-  <div></div>
+  <div>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>头像</th>
+        </tr>
+      </thead>
+      <!-- 需求 age headImgUrl 自定义指令 -->
+      <thead>
+        <tr v-for="(item, index) in list" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ item.name }}</td>
+          <td>
+            <slot name="age" :row="item"></slot>
+          </td>
+          <td>
+            <slot name="toux" :row="item"></slot>
+          </td>
+        </tr>
+      </thead>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      list: [
+        {
+          name: '小传同学',
+          age: 18,
+          headImgUrl:
+            'http://yun.itheima.com/Upload/./Images/20210303/603f2d2153241.jpg',
+        },
+        {
+          name: '小黑同学',
+          age: 25,
+          headImgUrl:
+            'http://yun.itheima.com/Upload/./Images/20210304/6040b101a18ef.jpg',
+        },
+        {
+          name: '智慧同学',
+          age: 21,
+          headImgUrl:
+            'http://yun.itheima.com/Upload/./Images/20210302/603e0142e535f.jpg',
+        },
+      ],
+    };
+  },
+};
 </script>
-
-<style></style>
